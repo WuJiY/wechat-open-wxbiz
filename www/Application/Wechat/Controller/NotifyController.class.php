2c86f3fa256e2889464fc980d3f3d2d084217da0
yhostc <yhostc@gmail.com>
2016-08-29 15:09:58 +0800

    update notify test

--- www/Application/Wechat/Controller/NotifyController.class.php
+++ www/Application/Wechat/Controller/NotifyController.class.php
@@ -4,12 +4,22 @@ use Think\Controller;
 class NotifyController extends Controller {
 
 
-    public function ticket(){
+
+	/**
+	 * 获取公众号授权
+	 * @return [type] [description]
+	 */
+    public function authorization(){
     	@file_put_contents(RUNTIME_PATH.'wechat_ticket.xml', @file_get_contents("php://input"));
 
     	echo 'SUCCESS';
     }
 
+    /**
+     * 获取公众号事件
+     * @param  string $app_id [description]
+     * @return [type]         [description]
+     */
     public function events($app_id=''){
     	@file_put_contents(RUNTIME_PATH."wechat_events_{$app_id}.xml", @file_get_contents("php://input"));
 
